90bcdf3abb6ff4a890fc0d727b1192187deb952e
aghilazghi <amineteclai@gmail.com>
2021-01-01 22:46:35 -0500

    Setup projects

---
+++ AzureServiceBusQueue/AzureServiceBusQueue.Shared/Models/CabinetOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AzureServiceBusQueue.Shared.Models
+{
+    public class CabinetOrder
+    {
+        [Required]
+        public string MaterialType { get; set; }
+        public string DoorSepcification { get; set; }
+        public int NumberOfDoors { get; set; }
+        public string DrawerSepcification { get; set; }
+        public int NumberOfDrawers { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AzureServiceBusQueue
+These console apps illustrate of utilizing Azure Service Bus Queue to create decoupled and independent modularized apps:
+- The AzureServiceBusQueue.Sender console app sends a message to Azure Service Bus Queue
+- The AzureServiceBusQueue.Reciever console app processes a message which is received from Azure Service Bus Queue.
